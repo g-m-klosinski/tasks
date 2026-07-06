5ec8643af6dc5266d5440a5a6875e19cfd0820ae
Gregory Klosinski <g.m.klosinski.wut@gmail.com>
2026-07-06 15:42:08 +0200

    Refactor

--- MyPlanTests/CRUDTest.cs
+++ MyPlanTests/CRUDTest.cs
@@ -2,27 +2,8 @@
 
 namespace MyPlanTests
 {
-    public class PlanManagerTest
+    public class CRUDTest
     {
-        [Fact]
-        public void ShouldExitInteractionOnQuitCommand()
-        {
-            var manager = new PlanManager.PlanManager("test-data/empty-plan.txt");
-
-            // Simulate user typing 'q' (plus Enter) to quit the interaction.
-            var originalIn = Console.In;
-            try
-            {
-                Console.SetIn(new System.IO.StringReader("q" + Environment.NewLine));
-                manager.startInteraction();
-                // If startInteraction returns without throwing, it exited on the quit command.
-            }
-            finally
-            {
-                Console.SetIn(originalIn);
-            }
-        }
-
         [Fact]
         public void ShouldPrintTasksOnPrintCommand()
         {
